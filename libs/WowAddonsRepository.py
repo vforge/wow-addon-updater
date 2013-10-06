7606b3fd0225ea308243a74c9b07b0d2b07d78cd
vforge <bartosz.bentkowski@gmail.com>
2013-10-06 17:02:31 +0200

    Finish get archive code, small main program refactoring

--- libs/WowAddonsRepository.py
+++ libs/WowAddonsRepository.py
@@ -1,3 +1,5 @@
+import requests
+import re
 from .WowUtils import WowUtils
 
 class WowAddonsRepository:
@@ -27,6 +29,34 @@ class WowRepositoryWowAce(WowAddonsRepository):
     def get_url_for(cls, addon):
         return "http://www.wowace.com/addons/%s/" % addon.curse_project_id
 
+    @classmethod
+    def get_full_url(cls, partial_url):
+        if partial_url[0:4] == 'http':
+          return partial_url
+        else:
+          return "http://www.wowace.com%s" % partial_url
+
+    @classmethod
+    def get_downloading_link(cls, addon):
+        url = cls.get_url_for(addon)
+        return cls.__download_chain(url)
+
+    @classmethod
+    def __download_chain(cls, url):
+        page = requests.get(url).content
+        regex = re.compile(bytes('user-action-download">.*href="(.*)">Download', 'utf-8'), re.I|re.DOTALL)
+        match = regex.search(page)
+        if match:
+            full_url = cls.get_full_url(str(match.group(1), encoding='UTF-8').strip())
+            print("Found match at: %s" % full_url)
+            if full_url[-4:] == '.zip':
+                return full_url
+            else:
+                return cls.__download_chain(full_url)
+        else:
+            return None
+
+
 class WowRepositoryCurseforge(WowAddonsRepository):
     def __init__(self):
         self.name = 'Curseforge'
@@ -39,6 +69,32 @@ class WowRepositoryCurseforge(WowAddonsRepository):
     def get_url_for(cls, addon):
         return "http://wow.curseforge.com/addons/%s/" % addon.curse_project_id
 
+    @classmethod
+    def get_full_url(cls, partial_url):
+        if partial_url[0:4] == 'http':
+          return partial_url
+        else:
+          return "http://wow.curseforge.com%s" % partial_url
+
+    @classmethod
+    def get_downloading_link(cls, addon):
+        url = cls.get_url_for(addon)
+        return cls.__download_chain(url)
+
+    @classmethod
+    def __download_chain(cls, url):
+        page = requests.get(url).content
+        regex = re.compile(bytes('user-action-download">.*href="(.*)">Download', 'utf-8'), re.I|re.DOTALL)
+        match = regex.search(page)
+        if match:
+            full_url = cls.get_full_url(str(match.group(1), encoding='UTF-8').strip())
+            print("Found match at: %s" % full_url)
+            if full_url[-4:] == '.zip':
+                return full_url
+            else:
+                return cls.__download_chain(full_url)
+        else:
+            return None
 
 class WowRepositoryTukui(WowAddonsRepository):
     def __init__(self):
--- wow-addon-updater.py
+++ wow-addon-updater.py
@@ -5,27 +5,42 @@ from libs.WowAddons import WowAddons
 from libs.WowUtils import WowUtils
 from libs.WowAddonsRepository import WowAddonsRepository
 
-def title():
-    print("=================")
-    print("WoW Addon Updater")
-    print("-----------------")
+def sep(style = '-'):
+	print(style * 30)
 
-def scan():
-    directory = 'AddOnsSample'
+def step0_title():
+	sep("=")
+	print("WoW Addon Updater")
+	sep()
 
-    if len(sys.argv) > 1:
-        directory = sys.argv[1]
+def step1_scan():
+	directory = 'AddOnsSample'
+	if len(sys.argv) > 1:
+		directory = sys.argv[1]
+	print("STEP 1: Scanning %s" % directory)
+	sep()
+	addons = WowAddons(directory)
+	sep()
+	return addons
 
-    print("SCANNING: %s" % directory)
-    print("-----------------")
-
-    return WowAddons(directory)
+def step2_get_archives(addons):
+	counter, size, links = 0, len(addons.addons), []
+	print("STEP 2: Finding archives")
+	sep()
+	for addon in addons.addons:
+		counter += 1
+		print("%d/%d: %s" % (counter, size, addon.name))
+		try:
+			repository = WowAddonsRepository.factory(addon)
+			links.append(repository.get_downloading_link(addon))
+		except ValueError:
+			print("Cannot find source for that :(")
+		finally:
+			sep()
+	return links
 
 # main functions
-title()
-addons = scan()
-counter, size = 0, len(addons.addons)
+step0_title()
+addons = step1_scan()
+links = step2_get_archives(addons)
 
-for addon in addons.addons:
-    counter += 1
-    print("%d/%d: %s" % (counter, size, addon.name))
